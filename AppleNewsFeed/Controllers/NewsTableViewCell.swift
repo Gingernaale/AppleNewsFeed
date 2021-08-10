60d2e6ae6f90cead809c2444fb37a5a7ba8cc8ba
Irina <missginger31@gmail.com>
2021-08-10 16:29:13 +0300

    Initial commit

---
+++ AppleNewsFeed/Controllers/NewsTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  NewsTableViewCell.swift
+//  AppleNewsFeed
+//
+//  Created by Irunya =} on 10/08/2021.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var newsTitleLabel: UILabel!
+    @IBOutlet weak var newsImageView: UIImageView!
+    
+}
